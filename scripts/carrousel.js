3ddc5d1bada09fdf52ab30074d72f18be4caa9b5
adeline bonnamour <ad.bonna@outlook.fr>
2024-10-16 16:34:58 +0200

    ajout des fichiers modifié du dernier commit

--- scripts/carrousel.js
+++ scripts/carrousel.js
@@ -1,15 +1,17 @@
-//Sélection des flèches
+//Sélection des flèches arrow_left et arrow_right
 const arrowleft = document.querySelector('.arrow_left');
 const arrowright = document.querySelector('.arrow_right');
 
-//ajout d'un eventlistener sur la flèches gauche
+//ajout d'un eventlistener sur la flèche gauche
 
 arrowleft.addEventListener('click', () => {
     console.log('flèche gauche cliquée');
+    alert('flèche gauche cliquée');
 });
 
-//ajout d'un eventlistener sur la flèches droite
+//ajout d'un eventlistener sur la flèche droite
 
 arrowright.addEventListener('click', () => {
     console.log('flèche droite cliquée');
+    alert('flèche droite cliquée');
 });
