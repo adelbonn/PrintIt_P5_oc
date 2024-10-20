9a252c4473b5bcbfa536ece36348269e821d8998
adeline bonnamour <ad.bonna@outlook.fr>
2024-10-20 12:48:27 +0200

    test

--- scripts/carrousel.js
+++ scripts/carrousel.js
@@ -1,17 +1,32 @@
 //Sélection des flèches arrow_left et arrow_right
-const arrowleft = document.querySelector('.arrow_left');
-const arrowright = document.querySelector('.arrow_right');
+const arrowleft = document.querySelector('.arrow_left')
+const arrowright = document.querySelector('.arrow_right')
 
 //ajout d'un eventlistener sur la flèche gauche
 
 arrowleft.addEventListener('click', () => {
-    console.log('flèche gauche cliquée');
-    alert('flèche gauche cliquée');
+    console.log('flèche gauche cliquée')
 });
 
 //ajout d'un eventlistener sur la flèche droite
 
 arrowright.addEventListener('click', () => {
-    console.log('flèche droite cliquée');
-    alert('flèche droite cliquée');
+    console.log('flèche droite cliquée')
 });
+
+//Sélection du container des bullets points
+
+const bullets = document.querySelector('.dots')
+
+//importation du tableau slides
+import { slides } from './slide.js';
+
+//ajout des bullets points
+slides.forEach((slide, index) => {
+    const bullet = document.createElement('div')
+    bullet.classList.add('dot')
+    if (index === 0) {
+        bullet.classList.add('active') //ajoute la classe pour la photo en cours
+    }
+    bullets.appendChild(bullet) //ajoute le bullet point dans le container
+    })
