901b21ee13a71cad9e73293a98215309eb689e71
adeline bonnamour <ad.bonna@outlook.fr>
2024-10-21 19:12:29 +0200

    ajout des flèches dans le html avec javascript

--- scripts/carrousel.js
+++ scripts/carrousel.js
@@ -1,5 +1,5 @@
 //Sélection des flèches arrow_left et arrow_right
-const arrowleft = document.querySelector('.arrow_left')
+/*const arrowleft = document.querySelector('.arrow_left')
 const arrowright = document.querySelector('.arrow_right')
 
 //ajout d'un eventlistener sur la flèche gauche
@@ -19,14 +19,14 @@ arrowright.addEventListener('click', () => {
 const bullets = document.querySelector('.dots')
 
 //importation du tableau slides
-import { slides } from './slide.js';
+import { slides } from './script.js';
 
 //ajout des bullets points
 slides.forEach((slide, index) => {
     const bullet = document.createElement('div')
     bullet.classList.add('dot')
     if (index === 0) {
-        bullet.classList.add('active') //ajoute la classe pour la photo en cours
+        bullet.classList.add('dot_selected') //ajoute la classe pour la photo en cours
     }
     bullets.appendChild(bullet) //ajoute le bullet point dans le container
-    })
+    })*/
---
+++ scripts/script.js
@@ -0,0 +1,56 @@
+ const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+//vérifie que le html est cahrgé avant d'executer le code qui suit
+document.addEventListener('DOMContentLoaded', () => {
+
+//ajout des flèches dans la balise banner
+
+//déclaration de la variable arrowLeft
+const arrowLeft = document.createElement('img');
+
+//definit le chemin de arrowLeft
+arrowLeft.src = '../assets/images/arrow_left.png';
+
+//ajout des classes arrow et arrow_left à arrowLeft
+arrowLeft.classList.add('arrow', 'arrow_left');
+
+ //sélection de l'élément banner + ajout de arrowLeft
+const banner = document.getElementById('banner')
+if (banner) {
+	banner.appendChild(arrowLeft)
+}
+ 
+
+//ajout de arrowRight
+
+//déclaration de la variables arrow_right
+const arrowRight = document.createElement('img');
+////définir le chemin vers mon image
+arrowRight.src = "../assets/images/arrow_right.png";
+//ajout des class arrow et arrow_right à arrowRight
+arrowRight.classList.add("arrow", "arrow_right");
+//ajout de l'enfant arrowRight à lélément div banner
+banner.appendChild(arrowRight);
+
+console.log(arrowLeft, arrowRight)
+
+
+
+})
